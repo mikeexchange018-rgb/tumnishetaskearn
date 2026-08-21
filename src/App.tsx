6ee01acdbb7e0b0784673e2d16b73135a1a58e98
mikeexchange018-rgb <mikeexchange018@gmail.com>
2026-08-21 07:29:58 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
 export default function App() {
-  return <h1 style={{color: 'white', textAlign: 'center', marginTop: '50px'}}>IT WORKS</h1>
+  return (
+    <h1 style={{ color: 'white', textAlign: 'center', marginTop: '50px' }}>
+      IT WORKS!!!
+    </h1>
+  )
 }
