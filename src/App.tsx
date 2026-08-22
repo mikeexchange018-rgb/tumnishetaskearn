b170bd847e497ee6f5de280552ed9cbdb500eb4f
mikeexchange018-rgb <mikeexchange018@gmail.com>
2026-08-22 14:04:26 +0100

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
+import Signup from './pages/Signup'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
+import Tasks from './pages/Tasks'
+import SubmitTask from './pages/SubmitTask'
+import Wallet from './pages/Wallet'
+import Admin from './pages/Admin'
+
+export default function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/tasks" element={<Tasks />} />
+          <Route path="/submit/:taskId" element={<SubmitTask />} />
+          <Route path="/wallet" element={<Wallet />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="*" element={<Navigate to="/signup" />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  )
+}
